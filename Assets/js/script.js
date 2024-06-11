1315258b841d8e3d8b36c0b1c587a21df1ef631d
Lverise <diego.pereira.200317@gmail.com>
2024-06-11 00:51:18 -0400

    Tipo de despacho
    Se agrega opción del despacho que tendrá el producto mediante un input radio de 2 opciones

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,9 +7,11 @@ addEventListener("load",()=>{
 function agregar(){
     let eTipo = document.getElementById("tipoProducto");
     let eColor = document.getElementById("colorProducto");
+    let eDescpacho = document.getElementById("tipoDespacho")
     let vTipo = eTipo.value
     let vColor = eColor.value
-    let objeto = {tipo:vTipo,color:vColor}
+    let vDespacho = eDescpacho.value
+    let objeto = {tipo:vTipo,color:vColor,despacho:vDespacho}
     
     agregarProducto(objeto).then(()=>{
         alert("Se agregó el producto")
